3a0d3233acd5d84fb7846a19935c7c2fc7b08de2
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-21 14:22:03 -0600

    insert python into template

--- app_name/views.py
+++ app_name/views.py
@@ -5,7 +5,7 @@ from .models import ToDoList, Item
 
 def index(response, id):
   ls = ToDoList.objects.get(id=id)
-  return render(response, "app_name/base.html", {})
+  return render(response, "app_name/show.html", {"ls":ls})
 
 def home(response):
   return render(response, "app_name/home.html", {})
